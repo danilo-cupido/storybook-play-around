2c3798d6c8bf60af889a6bcd9b191a65cf2401b0
danilo-cupido <danilocupido@gmail.com>
2022-01-06 19:12:05 +0100

    create several button stories

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -3,8 +3,42 @@ import Button from '../components/Button';
 export default {
   title: 'Button',
   component: Button,
+  argTypes: { handleClick: { action: 'it will do something' } },
 };
 
-export const Red = () => (
-  <Button label="Press me" backgroundColor="red" size="lg" />
-);
+const Template = (args) => <Button {...args} />;
+
+export const Red = Template.bind({});
+Red.args = {
+  backgroundColor: 'red',
+  label: 'Press Me',
+  size: 'md',
+};
+
+export const Green = Template.bind({});
+Green.args = {
+  backgroundColor: 'green',
+  label: 'Press Me',
+  size: 'md',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  backgroundColor: 'red',
+  label: 'Press Me',
+  size: 'sm',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  backgroundColor: 'red',
+  label: 'Press Me',
+  size: 'lg',
+};
+
+export const LongLabel = Template.bind({});
+LongLabel.args = {
+  backgroundColor: 'red',
+  label: 'Press this very looooooong button',
+  size: 'md',
+};
